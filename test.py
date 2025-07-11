9936ea7e365d56624c9a79c62745a53d5d86a5f8
ethernet007 <zenduki971@gmail.com>
2025-07-11 13:49:12 +0700

    Belajar2

---
+++ test.py
@@ -0,0 +1,12 @@
+# Contoh belajar kondisi (if-else) dalam Python
+
+nilai = 75
+
+if nilai >= 80:
+    print("Nilai Anda A")
+elif nilai >= 70:
+    print("Nilai Anda B")
+elif nilai >= 60:
+    print("Nilai Anda C")
+else:
+    print("Nilai Anda D")
